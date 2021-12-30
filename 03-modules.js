1f4b192dd932f33c6cf4195a3dfe1b6efbc6c3cc
Rahmat Ardiansyah <rahmat21@student.uir.ac.id>
2021-12-30 14:34:14 +0700

    first commit

---
+++ 03-modules.js
@@ -0,0 +1,11 @@
+// CommonJS, every file is module (by default)
+// Modules - Encapsulate Code (only share minimun)
+
+const names = require('./04-names');
+const sayHi = require('./05-utils');
+const data = require('./06-alternative-flavor');
+require('./07-mine-granade');
+
+// sayHi("rahmat");
+// sayHi(names.ardi);
+// sayHi(names.ardiansyah);
---
+++ 04-names.js
@@ -0,0 +1,8 @@
+// local
+const secret = 'SUPER SECRET'
+// share
+const ardi = "ardi";
+const ardiansyah = "ardiansyah";
+
+module.exports = { ardi, ardiansyah };
+
